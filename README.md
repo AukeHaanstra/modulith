0922b01156cf9dc53cf840f4595867fbb84eed40
Auke Haanstra <aukehaanstra@gmail.com>
2024-11-06 15:22:40 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Readme
+
+![Initialize the project](img/SpringInitializr.png)
---
+++ src/test/java/nl/pancompany/unicorn/modulith/ModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.pancompany.unicorn.modulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
